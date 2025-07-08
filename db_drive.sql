5db5708ca251fd29c39f3edca6d7c00a4eb54887
muhammadshidik <sidiksadar11@gmail.com>
2025-07-08 15:04:04 +0700

    lanjutan

--- db_drive.sql
+++ db_drive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jul 2025 pada 03.04
+-- Waktu pembuatan: 08 Jul 2025 pada 10.03
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -31,40 +31,66 @@ CREATE TABLE `level` (
   `id` int(11) NOT NULL,
   `level_name` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `level`
 --
 
-INSERT INTO `level` (`id`, `level_name`, `created_at`, `updated_at`) VALUES
-(1, 'Pimpinan', '2025-07-07 13:59:47', '2025-07-07 14:46:32'),
-(4, 'user', '2025-07-07 14:46:26', NULL);
+INSERT INTO `level` (`id`, `level_name`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Administrator', '2024-11-11 02:11:05', '2024-12-02 02:07:59', 0),
+(2, 'User', '2024-11-11 02:11:05', '2025-07-08 06:06:48', 0);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Struktur dari tabel `upload`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `upload` (
   `id` int(11) NOT NULL,
-  `id_level` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(30) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `file` varchar(50) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `profile_picture` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `deleted at` datetime DEFAULT NULL
+  `update_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data untuk tabel `upload`
 --
 
-INSERT INTO `users` (`id`, `id_level`, `name`, `email`, `password`, `created_at`, `updated_at`, `deleted at`) VALUES
-(7, 1, 'siddiq', 'admin@gmail.com', '123', '2025-07-07 14:00:08', NULL, NULL);
+INSERT INTO `upload` (`id`, `judul`, `file`, `deskripsi`, `profile_picture`, `created_at`, `update_at`) VALUES
+(6, 'anjay', 'user.sql', 'qqqqqqqqq', '', '2025-07-08 07:31:52', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `id_level` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `profile_picture` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `id_level`, `username`, `email`, `password`, `profile_picture`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, 'Muhammad Siddiq', 'admin@gmail.com', '123', 'profile_picture1.jpg', '2024-11-11 02:30:45', '2025-07-08 06:36:46', 0),
+(17, 2, 'admin12@gmail.com', 'sidik@gmail.com', '123', '', '2025-07-08 06:18:59', '2025-07-08 06:18:59', 0);
 
 --
 -- Indexes for dumped tables
@@ -77,11 +103,16 @@ ALTER TABLE `level`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `users`
+-- Indeks untuk tabel `upload`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_level` (`id_level`);
+ALTER TABLE `upload`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -91,23 +122,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `level`
 --
 ALTER TABLE `level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT untuk tabel `upload`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `upload`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- AUTO_INCREMENT untuk tabel `user`
 --
-
---
--- Ketidakleluasaan untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `level` (`id`);
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
